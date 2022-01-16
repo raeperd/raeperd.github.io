1f20a37122e6650fbfa01dc4a95dfe20da1e4bc0
raeperd <raeperd117@gmail.com>
2022-01-17 07:22:25 +0900

    #3 Implements /articles page

---
+++ pages/articles.tsx
@@ -0,0 +1,39 @@
+import NoteListView from '../components/NoteListView';
+import { NotePreview } from '../lib/note';
+import { getArticlePreviews } from '../lib/article';
+import { getPageSize, getSiteName } from '../lib/configuration';
+
+export default function ArticlesPage(
+  { title, articles, pageNumber, isFirstPage, isLastPage }: ArticlePageProps,
+) {
+  return (
+    <NoteListView
+      title={title}
+      notes={articles}
+      pageNumber={pageNumber}
+      isFirstPage={isFirstPage}
+      isLastPage={isLastPage}
+    />
+  )
+}
+
+type ArticlePageProps = {
+  title: string,
+  articles: NotePreview[],
+  pageNumber: number,
+  isFirstPage: boolean,
+  isLastPage: boolean
+}
+
+export async function getStaticProps(): Promise<{props: ArticlePageProps}> {
+  const pagedArticles = getArticlePreviews(1, getPageSize())
+  return {
+    props: {
+      title: getSiteName(),
+      articles: pagedArticles.notes,
+      pageNumber: pagedArticles.pageNumber,
+      isFirstPage: pagedArticles.isFirstPage,
+      isLastPage: pagedArticles.isLastPage,
+    },
+  }
+}
