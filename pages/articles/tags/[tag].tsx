b47391ad63f47eb1e67c6cf80a26a4b0e57d2cc2
raeperd <raeperd117@gmail.com>
2022-01-17 20:23:40 +0900

    #9 Implements /articles/tags/:tag

---
+++ pages/articles/tags/[tag].tsx
@@ -0,0 +1,55 @@
+import { getAllArticleTags, getArticlePreviewsByTag, NotePreview, Tag } from '../../../lib/note';
+import { getPageSize, getSiteName } from '../../../lib/configuration';
+import TagListHeader from '../../../components/TagListHeader';
+import NoteListView from '../../../components/NoteListView';
+
+export default function ArticleTagPage(
+  { tags, title, articles, tag, pageNumber, isFirstPage, isLastPage }: ArticleTagPageProps,
+) {
+  return (
+    <>
+      <TagListHeader tags={tags} />
+      <NoteListView
+        title={title}
+        notes={articles}
+        basePath={`/articles/tags/${tag}`}
+        pageNumber={pageNumber}
+        isFirstPage={isFirstPage}
+        isLastPage={isLastPage}
+      />
+    </>
+  )
+}
+
+type ArticleTagPageProps = {
+  tags: Tag[],
+  tag: string,
+  title: string,
+  articles: NotePreview[],
+  pageNumber: number,
+  isFirstPage: boolean,
+  isLastPage: boolean
+}
+
+export async function getStaticProps({ params }: {params: {tag: string}})
+  : Promise<{props: ArticleTagPageProps}> {
+  const pagedArticles = getArticlePreviewsByTag(params.tag, 1, getPageSize())
+  return {
+    props: {
+      tags: getAllArticleTags(),
+      tag: params.tag,
+      title: getSiteName(),
+      articles: pagedArticles.notes,
+      pageNumber: pagedArticles.pageNumber,
+      isFirstPage: pagedArticles.isFirstPage,
+      isLastPage: pagedArticles.isLastPage,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: getAllArticleTags().map((tag) => ({ params: { tag: tag.name } })),
+    fallback: false,
+  }
+}
