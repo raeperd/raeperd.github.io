208073341a7d9b136465af9f56e2ec7ec62ab671
raeperd <raeperd117@gmail.com>
2022-03-01 12:28:49 +0900

    Update nextjs version to 12

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,6 @@
-// jest.config.js
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({ dir: './' })
 
-const customJestConfig = {
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
-}
-
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig()
