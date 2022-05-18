ccafc5fd4ad8e765d3a0a7b3e587ce28fe2015d5
raeperd <raeperd117@gmail.com>
2022-05-18 18:47:59 +0900

    #20 Add coverage information on projects

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -15,8 +16,10 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  require('@cypress/code-coverage/task')(on, config)
+
+  return config
 }
