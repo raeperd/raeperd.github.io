aad79160d753dd7bc09a12e3b5e7ea9ef729d930
raeperd <raeperd117@gmail.com>
2022-06-03 00:58:39 +0900

    Refactor SyntaxHighlightedCodeBlock component

--- components/NoteView.tsx
+++ components/NoteView.tsx
@@ -7,6 +7,7 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { CodeProps } from 'react-markdown/lib/ast-to-react';
 import { Note } from '../lib/note'
 import TagListHeader from './TagListHeader';
 
@@ -23,7 +24,7 @@ export default function NoteView({ note }: NoteViewProps) {
       <TagListHeader tags={note.tags.map((tag) => ({ name: tag }))} basePath="/" />
       <section className="post-content">
         <ReactMarkdown
-          components={SyntaxHighlight}
+          components={{ code: SyntaxHighlightedCodeBlock }}
           // @ts-expect-error rehypeSlug and rehypeAutoLinkHeadings are not supporting typescript
           rehypePlugins={[rehypeRaw, rehypeKatex, rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]]}
           remarkPlugins={[remarkGfm, remarkMath]}
@@ -39,24 +40,20 @@ export interface NoteViewProps {
   note: Note,
 }
 
-const SyntaxHighlight: object = {
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-  code({ inline, className, children, ...props }) {
-    const match = /language-(\w+)/.exec(className || '')
-    return !inline && match ? (
-      <SyntaxHighlighter
-        style={vscDarkPlus}
-        language={match[1]}
-        PreTag="div"
-        {...props}
-      >
-        {String(children).replace(/\n$/, '')}
-      </SyntaxHighlighter>
-    ) : (
-      <code className={className} {...props}>
-        {children}
-      </code>
-    )
-  },
+function SyntaxHighlightedCodeBlock({ inline, className, children, ...props }: CodeProps) {
+  const match = /language-(\w+)/.exec(className || '')
+  return !inline && match ? (
+    <SyntaxHighlighter
+      style={vscDarkPlus}
+      language={match[1]}
+      PreTag="div"
+      {...props}
+    >
+      {String(children).replace(/\n$/, '')}
+    </SyntaxHighlighter>
+  ) : (
+    <code className={className} {...props}>
+      {children}
+    </code>
+  )
 }
