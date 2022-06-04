39825153a2aefe3b5080f06276980ac5593b2126
raeperd <raeperd117@gmail.com>
2022-06-04 20:05:04 +0900

    #47 Remove unused properties in css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <body data-menu="true">
+        <body>
           <Main />
           <NextScript />
         </body>
