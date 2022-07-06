a831878df5494d844159ae29cf00c9e4025146a3
raeperd <raeperd117@gmail.com>
2022-07-06 17:22:48 +0900

    #53 Refactor .algorithm-table into #note-table-nav

--- components/TaggedNoteTableView.tsx
+++ components/TaggedNoteTableView.tsx
@@ -2,56 +2,20 @@ import Link from 'next/link';
 import TagListHeader from './TagListHeader';
 import MainTitle from './MainTitle';
 import { TaggedNoteListViewProps } from './TaggedNoteListView';
-import { NoteListViewProps } from './NoteListView';
 import PaginationButton from './PaginationButton';
+import { NotePreview } from '../lib/note';
 
 export default function TaggedNoteTableView(
   {
-    tagPath, tags,
-    header, notes, pagePath, pageNumber, isFirstPage, isLastPage, lastPageNumber,
+    tagPath, tags, header, notes,
+    pagePath, pageNumber, isFirstPage, isLastPage, lastPageNumber,
   }: TaggedNoteTableViewProps,
 ) {
   return (
     <>
       <TagListHeader tags={tags} basePath={tagPath} />
-      <NoteTableView
-        header={header}
-        notes={notes}
-        pagePath={pagePath}
-        pageNumber={pageNumber}
-        isFirstPage={isFirstPage}
-        isLastPage={isLastPage}
-        lastPageNumber={lastPageNumber}
-      />
-    </>
-  )
-}
-
-export type TaggedNoteTableViewProps = TaggedNoteListViewProps
-
-function NoteTableView(
-  {
-    header,
-    notes, pagePath, pageNumber, isFirstPage, isLastPage, lastPageNumber,
-  }: NoteListViewProps,
-) {
-  return (
-    <>
       {header && <MainTitle title={header} />}
-      <table className="algorithm-table">
-        <tr>
-          <th>Problem</th>
-          <th>Idea</th>
-        </tr>
-        {notes.map((note) => (
-          <tr key={note.staticPath}>
-            <td className="title"><Link href={`/${note.staticPath}`}>{note.title}</Link></td>
-            <td>
-              <Link href={`/${note.staticPath}`}><a data-cy="note-link">{note.idea}</a></Link>
-            </td>
-          </tr>
-        ))}
-      </table>
+      <NoteTableNav notes={notes} />
       <PaginationButton
         isFirstPage={isFirstPage}
         isLastPage={isLastPage}
@@ -61,4 +25,25 @@ function NoteTableView(
       />
     </>
   )
+}
+
+export type TaggedNoteTableViewProps = TaggedNoteListViewProps
+
+function NoteTableNav({ notes }: {notes: NotePreview[]}) {
+  return (
+    <table id="note-table-nav">
+      <tr>
+        <th>Problem</th>
+        <th>Idea</th>
+      </tr>
+      {notes.map((note) => (
+        <tr key={note.staticPath}>
+          <td className="title"><Link href={`/${note.staticPath}`}>{note.title}</Link></td>
+          <td>
+            <Link href={`/${note.staticPath}`}><a data-cy="note-link">{note.idea}</a></Link>
+          </td>
+        </tr>
+      ))}
+    </table>
+  );
 }
