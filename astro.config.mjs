93026eb282e63566ee6d867fc37a9f5ab33f503f
raeperd <raeperd117@gmail.com>
2022-09-27 22:41:10 +0900

    feat: Implements PostLayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,15 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [defaultLayoutPlugin],
+    extendDefaultPlugins: true
+  },
 });
+
+function defaultLayoutPlugin () {
+  return function (tree, file) {
+    file.data.astro.frontmatter.layout = '@layouts/PostLayout.astro'
+  }
+}
