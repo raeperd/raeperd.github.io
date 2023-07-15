8a92f766a91d0b4e1dec9e1acea562f708f7ef4f
raeperd <raeperd117@gmail.com>
2023-07-15 23:25:34 +0900

    chore: Remove all contents from dir

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "raeperd.github.io";
-export const SITE_DESCRIPTION = "Technical blog by raeperd";
