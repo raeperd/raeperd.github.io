abad5154ec764bee4240ecca28ba930a4792c0b0
raeperd <raeperd117@gmail.com>
2023-07-15 13:25:51 +0900

    fix: Fix import error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
