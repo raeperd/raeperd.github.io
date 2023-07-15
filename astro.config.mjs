24b09c28377de8edc33bc44768ca150430fa2baf
raeperd <raeperd117@gmail.com>
2023-07-15 13:34:06 +0900

    test: Fail build for test discord message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 
-import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
