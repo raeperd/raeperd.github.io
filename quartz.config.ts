af2db549bcbeaf08da2986382ab6f06af7486ea8
raeperd <raeperd117@gmail.com>
2024-05-10 01:08:51 +0900

    Quartz sync: May 10, 2024, 1:08 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "raeperd.github.io",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
